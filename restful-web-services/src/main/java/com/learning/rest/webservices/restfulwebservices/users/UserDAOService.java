c63698971b43577e40ae7f7ad41b410c3af1bd0d
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-07 20:32:30 -0500

    Creating user beans and user DAO service

---
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/users/UserDAOService.java
@@ -0,0 +1,29 @@
+package com.learning.rest.webservices.restfulwebservices.users;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDAOService {
+    //to store all user details in the Database
+
+    //UserDAOService -> static list
+    private static List<Users> users = new ArrayList<>();
+
+    static {
+        users.add(new Users(1, "George", LocalDate.now().minusYears(25)));
+        users.add(new Users(2, "Olivier", LocalDate.now().minusYears(30)));
+        users.add(new Users(3, "Francois", LocalDate.now().minusYears(35)));
+    }
+
+    //implement method to be able to retrieve all users
+    public List<Users> findAllUsers() {
+        return users;
+    }
+    //to be able to save the details of a specific user
+
+    //to be able to retrieve details of a specifc user
+}
