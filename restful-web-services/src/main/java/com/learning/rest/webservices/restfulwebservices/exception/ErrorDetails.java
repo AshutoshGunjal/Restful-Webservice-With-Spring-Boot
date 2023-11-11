b43bc200d7bff2cbd3e84ce1a78802a87ad05a0e
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-10 21:45:17 -0500

    Implementing Generic Exception Handling

---
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.learning.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+
+    private String details;
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+
+}
