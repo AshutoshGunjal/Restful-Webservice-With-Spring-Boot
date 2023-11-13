76f52466058b958e48141eed8a7c7b03df25924b
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-11-13 12:58:13 -0500

    Connecting REST API to H2 using JPA and Hibernate
    Creating User Entity and Some Test Data

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/users/Users.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/users/Users.java
@@ -1,12 +1,18 @@
 package com.learning.rest.webservices.restfulwebservices.users;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class Users {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should be at least 2 characters")
     @JsonProperty("user_name")
